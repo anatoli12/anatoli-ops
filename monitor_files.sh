9142420d9b68f6ca1a507978790cb7fc0fc0a098
a.dimov <a.dimov@tinqin.com>
2023-11-06 12:12:04 +0200

    Added missed quotes etc. and documentation in the readme

--- monitor_files.sh
+++ monitor_files.sh
@@ -1,4 +1,4 @@
-#!binbash
+#!/bin/bash
 
 # Get the current date and time
 current_datetime=$(date +%Y-%m-%d %H%M%S)
@@ -7,13 +7,9 @@ current_datetime=$(date +%Y-%m-%d %H%M%S)
 open_files_count=$(lsof  wc -l)
 
 # Output the date and time, and the count of open files
-echo Date and Time $current_datetime
-echo Open files count $open_files_count
+echo "Date and Time: $current_datetime"
+echo "Open files count: $open_files_count"
 
-# List all open files
-#echo List of open files
-#lsof
-
-# If you want to log this to a file instead of displaying it, you can redirect the output to a file like this
-echo Date and Time $current_datetime  open_files_log.txt
-echo Open files count $open_files_count  open_files_log.txt
+# Output the logs to a text file
+echo "Date and Time: $current_datetime" >> open_files_log.txt
+echo "Open files count: $open_files_count" >> open_files_log.txt
